1a28abb20f861d6ef5d5bc2a506a02c799bc5256
dourgey <dourgey@gmail.com>
2024-06-11 21:54:03 +0800

    mod: update module init

--- src/__init__.py
+++ src/__init__.py
@@ -2,5 +2,6 @@ import sys
 import os
 sys.path.append(os.path.abspath(__file__))
 
-from .ChatAPI import BaiduChatAPI, TencentChatAPI, ZhiPuChatAPI, YiChatAPI, BaichuanChatAPI, QwenChatAPI, MoonshotChatAPI, DeepSeekChatAPI
+from .ChatAPI import (BaiduChatAPI, TencentChatAPI, ZhiPuChatAPI, YiChatAPI, BaichuanChatAPI, QwenChatAPI,
+                      MoonshotChatAPI, DeepSeekChatAPI, LarkChatAPI, SparkChatAPI)
 from .Enum import MODEL, APIKEY
